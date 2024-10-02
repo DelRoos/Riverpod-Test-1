4d2e31717101d872a9f6793d9d3a0b751123920a
louenkamfrank <louenkamfrank@gmail.com>
2024-10-01 23:31:05 -0700

    use consumer widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_test/screens/home/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
